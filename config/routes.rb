71750ffae32f9fc4fcad3a47a2d1f559dce4e3ef
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-06-09 22:59:17 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show]
+  #This will create routing methods for posts that we can utilize in our views and controllers.
+  
 end
